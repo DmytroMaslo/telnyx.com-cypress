28f5d7c5ab23ce3b4d66a88bb6fe79b27b65e734
Dmytro Maslo <maslo.dmitry@gmail.com>
2022-11-28 14:39:19 +0200

    check slider Make Outbound

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -16,7 +16,6 @@ context('Tests on main page', () => {
     MainPage.secondTitle.should('contain.text',this.data.main.secondTitle)
   })
   it('Check Switch + Save with Telnyx, Make outbound calls', function(){
-    
     MainPage.moveSlider()
     MainPage.saveUp.should('contain.text','4353')
   })
